ceeeddd55d0335ff4b8e193e49a29a4b4726209f
Snow_Leopard <30438336+SnowleopardV@users.noreply.github.com>
2018-07-24 22:16:38 +0800

    添加注释
    添加注释

--- js/dist/alert.js
+++ js/dist/alert.js
@@ -1,7 +1,7 @@
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
+/*添加个注释*/
 /**
  * --------------------------------------------------------------------------
  * Bootstrap (v4.1.2): alert.js
